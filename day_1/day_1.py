ccbed17bca25d2db1f03d6e50f1f614fc1da96ff
dzabila <giorgi.dzabilashvili.1@btu.edu.ge>
2025-03-25 07:56:26 +0400

    Day 1 commit

---
+++ day_1/day_1.py
@@ -0,0 +1,41 @@
+# #  \n starts text on new line
+# print("Hello world!\nHello world!\nHello World!")
+
+# # concatenation
+# print("Hello" + " " + "Gorge")
+
+# # input function able you to  enter some data
+
+# input('What is your name? ::::')
+
+# # we can input function inside function
+# print("Hello" + " " + input("What is your name? :") + "!")
+
+#Variables 
+
+# name = "aleqsandre"
+# print(name)
+# # len() Funqciit vigebt stringis sigrdzes
+# print(len(name))
+
+# userName = input("what is your name?")
+# length = len(userName)
+# print(length)
+
+# Variable Naming
+# 1.Make code readable
+# 2.underscore-s viyenebt gamoyofistvis: user_name
+# 3.enis saxelebi ar gamoiyeno cvladebis saxelebad
+# 4.nameError agdebs mashin roca arasworad gamoiyeneb cvladis saxels
+
+
+# Day 1 project -- band name generator
+
+print("Welcome to the Band Name Generator.")
+
+city = input("What's the name of the city you grew up in?\n")
+
+pet_name = input("What's yor pet's name?\n")
+
+text = "Your band name could be" + " " + city + " " + pet_name
+print(text)
