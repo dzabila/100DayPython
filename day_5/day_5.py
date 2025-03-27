eeeb7a7519baae8d2c41114de88b016020e35b16
dzabila <giorgi.dzabilashvili.1@btu.edu.ge>
2025-03-27 19:10:41 +0400

    day 5 practice

---
+++ day_5/day_5.py
@@ -0,0 +1,28 @@
+# fruits = ["Apple", "Peach", "Pear"]
+# # am lupit shegvidzlia gadaurbinot lists 
+# for fruit in fruits:
+#   print(fruit)
+#   print(fruit + " Pie")
+# sum = 0
+# student_scores = [180, 124, 165, 173, 189, 169, 146]
+# # sum() funqciit shegvidzlia listis jami mivigot
+# print(sum(student_scores))
+
+# # max() funqciit shegvidzlia listis maqsimaluri elementi wamovigot\
+# print(max(student_scores))
+
+# max = student_scores[0]
+# for num in student_scores:
+#   if num > max:
+#     max = num
+
+
+# print(max)
+
+
+# range(a, b, c) a-s chatvlit b-mde daprintavs yvela cifrs, xolo c aris biji
+sum = 0
+for number in range(0, 101):
+  sum +=number
+
+print(sum)
