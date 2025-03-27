f9f7b75c16842c3913a8be45671da8d883396c5e
dzabila <giorgi.dzabilashvili.1@btu.edu.ge>
2025-03-27 10:06:14 +0400

    day 4 practice

---
+++ day_4/day_4.py
@@ -0,0 +1,52 @@
+# Randomisation
+import random
+import for_import
+
+
+
+# rand_num = random.randint(1, 10)
+# print(rand_num)
+# print(for_import.my_favorite_number)
+
+# # es agenerirebs random float ricxvs 0 dan ertamde
+# random_number_0_to_1 = random.random() * 10
+
+# # sheidzleba aman ertic da atic amoagdos minusi eg aqvs
+# random_float = random.uniform(1, 10)
+# print(random_float)
+# random_number = random.randint(1, 2)
+# if random_number == 1:
+#   print("Heads")
+# else:
+#   print("Tails")
+
+### lists 
+# fruits = ["apple", "Cherry", "Pear"]
+
+
+# states_of_america = ["Delaware", "Pennsylvainai", "Missisipi"]
+
+# print(states_of_america[-1])
+# # konkretul indeqsze mnishvnelobis shecvla an damateba
+# states_of_america[1] = "Pencilvania"
+# # list is boloshi elementis damateba
+# states_of_america.append("Georgia")
+# print(states_of_america)
+# # list is gafartoeba
+# states_of_america.extend(["New Jeresey", "Colorado"])
+# print(states_of_america)
+
+
+# # Task
+# friends = ["Alice", "Bob", "Charlie", "Davaid", "Emanuel"]
+
+# random_number = random.randint(0, len(friends))
+# print(friends[random_number])
+
+# # second variant
+# print(random.choice(friends))
+
+# list nested in list
+fruits = ["Strawberries", "Nectarines", "Apples", "Grapes"]
+vegetables = ["Spinach", "Kale", "Tomatoes"]
+dirty_dozen = [fruits, vegetables]
