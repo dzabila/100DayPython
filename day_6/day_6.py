d46d67d94fd4795550243c48f7a887e75ac5393d
dzabila <giorgi.dzabilashvili.1@btu.edu.ge>
2025-03-28 18:32:40 +0400

    day  6 practice

---
+++ day_6/day_6.py
@@ -0,0 +1,15 @@
+# Defining and Calling functions
+
+def my_function():
+  print("Hello")
+  print("Bye")
+
+my_function()
+
+# While loops
+i = 10
+while i > 0:
+  print("Hello world")
+  i -= 1
+
+  
